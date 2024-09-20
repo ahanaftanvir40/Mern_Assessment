d9798fa2d85387139d9ea19a8fc5c17770c074f0
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-09-20 18:17:56 +0600

    up

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: '*'
 }))
 
 const port = process.env.PORT || 5173
